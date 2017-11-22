9ee49c93d0d6dd9cd79db9210808f748b9eafc02
Daniel Wang <dawa2101@hedenius.it.uu.se>
2017-11-22 16:11:21 +0100

    I landet ingenstans

--- source/ParserDriver.java
+++ source/ParserDriver.java
@@ -3,13 +3,11 @@ import java.io.IOException;
 class ParserDriver{
     public static void main(String[] args){
 
-
         System.out.println("Welcome to the parser!");
         System.out.print("Please enter an expression: ");
 
         while(true) {
             try{
-
                 Parser p = new Parser();
                 System.out.print("\n? ");
                 Sexpr result = p.expression();
@@ -17,7 +15,6 @@ class ParserDriver{
                 Sexpr total = result.eval();
                 System.out.println("evaluated to: " + total);
 
-            
             }catch(SyntaxErrorException e){
                 System.out.print("Syntax Error: ");
                 System.out.println(e.getMessage());
