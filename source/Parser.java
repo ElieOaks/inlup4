512d4991629e5f2def565f4314d168233bdecbdd
Daniel Wang <dawa2101@fredholm.it.uu.se>
2017-11-27 15:29:59 +0100

    mergen kärnvapenbombade parser, fixade det nu.

--- source/Parser.java
+++ source/Parser.java
@@ -1,46 +1,3 @@
-    /**
-
-
-     * Checks if the next token is a '+'/'-'/'='.
-     * @return a Sexpr expression. If there is a '+'/'-'/'=', it returns an instance of Addition/Subtraction/Assignment. 
-     * Throws exception if the right side expression is incorrect.
-     */
-    /**
-     * Checks if the next token is a variable or not.
-     * @return a Sexpr expression. If token is a single char, then returns an instance of Variable.
-     * Throws exception if the next token is not assigned to a variable.
-     */
-
-    /**
-     * Checks if the following token is a multiplication operator.
-     * @return a Sexpr expression. If token is a '*'/'/', it returns a Multiplicaiton/Division Sexpr. 
-     * throws exception if right side term, is not a valid expression or number.
-     */
-
-    /**
-     * Checks if the following character is the beginning of a number or an expression between paranthesees. 
-     * @return a Sexpr expression. 
-     * throws exception if paranthesees are not matching. 
-     */
-
-    /** 
-     * Checks if the next token is a variable or a number.
-     * @return a Sexp expression of a Constant or returns a call of variableorUnary()
-     * Throws exception if it is neither a word of a number.
-     */
-
-    /**
-     * Checks if there are matching paranthesees
-     * Throws exception if parantheses do not match. 
-     * @return the expression between the matching paranthesees.
-     */
-
-    /**
-     * Checks if the a string is variable, or a unary expression
-     * Throws exception when: length > 1 && not a Unary expression.
-     * @return the variableor Unary expression.
-     */
-
 import java.io.StreamTokenizer;
 import java.io.IOException;
 
@@ -48,11 +5,11 @@ class Parser{
     StreamTokenizer st;
 
     /**
-     * Constructor
-     * Input stream is from prompter
-     * '-' is read as an ordinary char.
-     * EOL is a token
-     */
+     *      * Constructor
+     *           * Input stream is from prompter
+     *                * '-' is read as an ordinary char.
+     *                     * EOL is a token
+     *                          */
     public Parser(){
         st = new StreamTokenizer(System.in);
         st.ordinaryChar('-');
@@ -64,106 +21,160 @@ class Parser{
         st.nextToken();
         if (st.ttype == st.TT_WORD) {
             switch(st.sval) {
-            case "vars":
-                return new Vars();
-            case "quit":
-                return new Quit();
+                case "vars":
+                    return new Vars();
+                case "quit":
+                    return new Quit();
             }
-            
+
         }
         st.pushBack();
         return expression();
     }
+
     /**
-     * @return a Sexpr expression. If there is a '+'/'-', it returns a Addition/Subtraction Sexpr. 
-     */
+     *      * Checks if the next token is a '+'/'-'/'='.
+     *           * @return a Sexpr expression. If there is a '+'/'-'/'=', it returns an instance of Addition/Subtraction/Assignment. 
+     *                * Throws exception if the right side expression is incorrect.
+     *                     */
     private Sexpr expression() throws IOException{
-        Sexpr expression = term();
+        Sexpr expr = termMultOrDiv();
         st.nextToken();
         while (st.ttype == '+' || st.ttype == '-' || st.ttype == '=') {
             if(st.ttype == '+'){
-                Sexpr termRight = term();
-                expression = new Addition(expression, termRight);
-            } else if (st.ttype == '-'){
-                Sexpr termRight = term();
-                expression = new Subtraction(expression, termRight);
-            } else {
-                Sexpr termRight = variable();
-                if (termRight.toString().length() == 1)
-                expression = new Assignment(expression, termRight);//Set conditions in Binary?
+                Sexpr rightTerm = termMultOrDiv();
+
+                if(rightTerm == null) {
+                    throw new SyntaxErrorException("Incorrect right side term");
+                }else{
+                    expr = new Addition(expr, rightTerm);
+                }
+
+            }else if(st.ttype == '-'){
+                Sexpr rightTerm = termMultOrDiv();
+
+                if(rightTerm == null) {
+                    throw new SyntaxErrorException("Incorrect right side term");
+                }else{
+                    expr = new Subtraction(expr, rightTerm);
+                }
+
+            }else {
+                Sexpr rightTerm = assignment();
+
+                if(rightTerm == null) {
+                    throw new SyntaxErrorException("Incorrect right side term");
+                }else{
+                    expr = new Assignment(expr, rightTerm);
+                }
+
             }
             st.nextToken();
         }
         st.pushBack();
-        return expression;
+        return expr;
     }
 
+
     /**
-     * @return a Sexpr expression. If there is a '*', it returns a Multiplicaiton Sexpr. 
-     */
-    private Sexpr term() throws IOException{
-        Sexpr expression = factor();
+     *      * Checks if the next token is a variable or not.
+     *           * @return a Sexpr expression. If token is a single char, then returns an instance of Variable.
+     *                * Throws exception if the next token is not assigned to a variable.
+     *                     */
+    private Sexpr assignment() throws IOException {  //how to get left value, read 3 tokens?
+        Sexpr rightTerm;
         st.nextToken();
-        if (st.ttype == '*'){
-            Sexpr factorRight = term();
-            expression = new Multiplication(expression, factorRight);
+        if(st.ttype == st.TT_WORD) {
+            return new Variable(st.sval);
         }
-        else if (st.ttype == '/') {
-            Sexpr factorRight = term();
-            expression = new Division(expression, factorRight); 
-        }        
-        st.pushBack();
-        return expression;
+        else {
+            throw new SyntaxErrorException("Incorrect assignment, expected variable");
+        }
+    }
+
+
+    /**
+     *      * Checks if the following token is a multiplication operator.
+     *           * @return a Sexpr expression. If token is a '*'/'/', it returns a Multiplicaiton/Division Sexpr. 
+     *                * throws exception if right side term, is not a valid expression or number.
+     *                     */
+    private Sexpr termMultOrDiv() throws IOException{ //needs a better name
+        Sexpr expr = factor();
+        if(st.nextToken() == '*') {
+            st.pushBack();
+            while (st.nextToken() == '*'){
+                Sexpr factorRight = termMultOrDiv();
+
+                if(factorRight == null) { 
+                    throw new SyntaxErrorException("Incorrect right side expression");
+                }else{
+                    expr = new Multiplication(expr, factorRight);
+                }
+
+            }
+            st.pushBack();
+        } 
+        if(st.nextToken() == '/') {
+            st.pushBack();
+            while (st.nextToken() == '/'){
+                Sexpr factorRight = termMultOrDiv();
+
+                if(factorRight == null) {
+                    throw new SyntaxErrorException("Incorrect right side expression");
+                }else{
+                    expr = new Division(expr, factorRight);
+                }
+
+            }
+            st.pushBack();
+        } 
+        return expr;
     }
 
     /**
-     * Checks if the following character is the beginning of a number or an expression between paranthesees. 
-     * @return a Sexpr expression. 
-     * throws exception if paranthesees are not matching. 
-     */
+     *      * Checks if the following character is the beginning of a number or an expression between paranthesees. 
+     *           * @return a Sexpr expression. 
+     *                * throws exception if paranthesees are not matching. 
+     *                     */
     private Sexpr factor() throws IOException{
         Sexpr result;
         if(st.nextToken() != '('){
             st.pushBack();
             result = number();
-        }else{
+        } else {
             result = expression();
-            if(st.nextToken() != ')'){
+            if (st.nextToken() != ')') {
                 throw new SyntaxErrorException("expected ')'");
             }
         }
         return result;
     }
 
-    /**
-     * 
-     * @return a Sexp expression of a Constant or returns a call of variableorUnary()
-     * Throws exception if it is neither a word of a number.
-     */
+    /** 
+     *      * Checks if the next token is a variable or a number.
+     *           * @return a Sexp expression of a Constant or returns a call of variableorUnary()
+     *                * Throws exception if it is neither a word of a number.
+     *                     */
     private Sexpr number() throws IOException{
-        
+
         if(st.nextToken() == st.TT_WORD) {
             st.pushBack();
             return variableOrUnary();
         }
         st.pushBack();
-        if (st.nextToken() == '-') {
-            Sexpr argument = checkParanthesees();
-            return new Negation(argument);
-        }
-        st.pushBack();
+
         if(st.nextToken() != st.TT_NUMBER){
             throw new SyntaxErrorException("Expected number");
         }
         return new Constant(st.nval);
-       
+
     }
 
     /**
-     * Checks if there are matching paranthesees
-     * Throws exception if parantheses do not match. 
-     * @return the expression between the matching paranthesees.
-     */
+     *      * Checks if there are matching paranthesees
+     *           * Throws exception if parantheses do not match. 
+     *                * @return the expression between the matching paranthesees.
+     *                     */
     private Sexpr checkParanthesees() throws IOException{
         if(st.nextToken() != '('){
             throw new SyntaxErrorException("expected '('");
@@ -176,45 +187,48 @@ class Parser{
     }
 
     /**
-     * Checks if the a string is variable, or a unary expression
-     * Throws exception when: length > 1 && not a Unary expression.
-     * @return the variableor Unary expression.
-     */
+     *      * Checks if the a string is variable, or a unary expression
+     *           * Throws exception when: length > 1 && not a Unary expression.
+     *                * @return the variableor Unary expression.
+     *                     */
     private Sexpr variableOrUnary() throws IOException{
         if(st.nextToken() != st.TT_WORD && st.sval.length() != 3 && st.sval.length() != 1) { 
             throw new SyntaxErrorException("Expected a variable or unirary expression");
         }
         else if(st.sval.length() == 3) {
             switch(st.sval) {
-            case "cos":
-                Sexpr cosArgument = checkParanthesees();
-                return new Cos(cosArgument);
-            case "sin":
-                Sexpr sinArgument = checkParanthesees();
-                return new Sin(sinArgument);
-            case "exp":
-                Sexpr expArgument = checkParanthesees();
-                return new Exp(expArgument);
-            case "log":
-                Sexpr logArgument = checkParanthesees();
-                return new Log(logArgument);
-            default:
-                throw new SyntaxErrorException("Unknown Expression");
-            }                
-        }
-        else {
-            st.pushBack();
-            return variable();
-        }
-    }
+                case "cos":
+                    Sexpr cosArgument = checkParanthesees();
+                    return new Cos(cosArgument);
+                case "sin":
+                    Sexpr sinArgument = checkParanthesees();
+                    return new Sin(sinArgument);
+                case "exp":
+                    Sexpr expArgument = checkParanthesees();
+                    return new Exp(expArgument);
+                case "log":
+                    Sexpr logArgument = checkParanthesees();
+                    return new Log(logArgument);
+                default:
+                    throw new SyntaxErrorException("Unknown Expression");
+            }
 
-    private Sexpr variable() throws IOException{
-        if (st.nextToken() == st.TT_WORD && st.sval.length() == 1) {
-            return new Variable(st.sval);
-        }
-        throw new SyntaxErrorException("Expected a variable");
+        } 
+        else
+            if (st.sval == "-") {
+                return new Negation(expression());
+            }
+        return new Variable(st.sval);
     }
 
 
-    
+
+    public class SyntaxErrorException extends RuntimeException{
+        public SyntaxErrorException(){
+            super();
+        }
+        public SyntaxErrorException(String msg){
+            super(msg);
+        }
+    }
 }
