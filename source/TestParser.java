be3276e57de3380bad687034656c36d453205f3a
Daniel Wang <dawa2101@arrhenius.it.uu.se>
2017-12-03 19:25:57 +0100

    Cleared file of bugs. TODO:add some more tests.

--- source/TestParser.java
+++ source/TestParser.java
@@ -1,71 +1,72 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.*;
+import java.io.IOException;
 
 
-public class ParserTest {
+public class TestParser {
 
     @Test
-    public void addVariableTest {
+    public void addVariableTest() throws IOException{
         Sexpr actual = Parser.parse("a + b");
         Sexpr expected = new Addition(new Variable("a"), new Variable("b"));
-        Assert.assertEquals(expected.toString(), actual.toString(), "a + b");
+        assertEquals(expected.toString(), actual.toString(), "a + b");
     }
 
 	@Test
-	public void subtractionVariableTest {
+	public void subtractionVariableTest() throws IOException{
 		Sexpr actual = Parser.parse("a - b");
 		Sexpr expected = new Subtraction(new Variable("a"), new Variable("b"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "a - b");
+		assertEquals(expected.toString(), actual.toString(), "a - b");
 	}
 
 	@Test
-	public void addConstantTest {
+	public void addConstantTest() throws IOException {
 		Sexpr actual = Parser.parse("1 + 2");
-		Sexpr expected = new Addition(new Constant("1"), new Constant("2"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "1 + 2");
+		Sexpr expected = new Addition(new Constant(1), new Constant(2));
+		assertEquals(expected.toString(), actual.toString(), "1 + 2");
 	}
 
 	@Test
-	public void subtractConstantTest {
+	public void subtractConstantTest() throws IOException{
 		Sexpr actual = Parser.parse("1 - 2");
-		Sexpr expected = new Subtraction(new Constant(("1"), new Constant("2"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "1 - 2");
+		Sexpr expected = new Subtraction(new Constant(1), new Constant(2));
+		assertEquals(expected.toString(), actual.toString(), "1 - 2");
 	}
 
 	@Test
-	public void multiplicationConstantTest {
+	public void multiplicationConstantTest() throws IOException{
 		Sexpr actual = Parser.parse("7 * 3");
-		Sexpr expected = new Multiplication(new Constant("7"), new Constant("3"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "7 * 3");
+		Sexpr expected = new Multiplication(new Constant(7), new Constant(3));
+		assertEquals(expected.toString(), actual.toString(), "7 * 3");
 	}
 
 	@Test
-	public void divisionConstantTest {
+	public void divisionConstantTest() throws IOException{
 		Sexpr actual = Parser.parse("7 / 3");
-		Sexpr expected = new Division(new Constant("7"), new Constant("3"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "7 / 3");
+		Sexpr expected = new Division(new Constant(7), new Constant(3));
+		assertEquals(expected.toString(), actual.toString(), "7 / 3");
 	}
 
 	@Test
-	public void parenthesisTest1 {
+	public void parenthesisTest1() throws IOException{
 		Sexpr actual = Parser.parse("((2 - 4) * 3)");
-		Sexpr expected = new Multiplication(new Subtraction(new Constant("2"), new Constant("4")), new Constant("3"));
-		Assert.assertEquals(expected.toString(), actual.toString(), "((2 - 4) * 3)");
+		Sexpr expected = new Multiplication(new Subtraction(new Constant(2), new Constant(4)), new Constant(3));
+		assertEquals(expected.toString(), actual.toString(), "((2 - 4) * 3)");
 	}
 
-
+/*
 	@Test
 	public void parenthesisTest2 {
 		Sexpr actual = Parser.parse("Log
-	
+*/	
 
 
 	@Test
-	public unaryTest1 {
+	public void unaryTest1() throws IOException{
 		Sexpr actual = Parser.parse("sin(sin(7/2))");
-		Sexpr expected = new Sin(new Sin(new Division(7, 2)));
-		Assert.assertEquals(expected.toString(), actual.toString(), "sin(sin(7/2))");
+		Sexpr expected = new Sin(new Sin(new Division(new Constant(7),new Constant(2))));
+		assertEquals(expected.toString(), actual.toString(), "sin(sin(7/2))");
 	}
 	
 /*	@Test
